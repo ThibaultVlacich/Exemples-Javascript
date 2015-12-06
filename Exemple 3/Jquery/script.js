20078f90b2632635b0f1c539be673bded3c03472
Thibault Vlacich <thibault.vlacich@gmail.com>
2015-12-06 20:43:46 +0100

    Ajout du troisième exemple en jQuery

---
+++ Exemple 3/Jquery/script.js
@@ -0,0 +1,61 @@
+/**
+ * Permet de valider si un string est une adresse email valide
+ *
+ * @param   email string  La chaine de caractère à vérifier
+ *
+ * @return  bool
+ */
+function validateEmail(email) {
+   var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+   return re.test(email);
+}
+
+$('input').on('change keyup', function() {
+  var $this = $(this),
+      $nickname = $('#nickname'),
+      $email = $('#email'),
+      $password = $('#password'),
+      $password_confirm = $('#password_confirm')
+      $submit_button = $('#submit');
+
+  if (
+    ($this.is($nickname) && $this.val().length > 3)
+    || ($this.is($email) && validateEmail($this.val()))
+  ) {
+    $this.addClass('good');
+    $this.removeClass('error');
+  } else if ($this.is($password) || $this.is($password_confirm)) {
+    if ($password.val() == $password_confirm.val()) {
+      $password.addClass('good');
+      $password_confirm.addClass('good');
+
+      $password.removeClass('error');
+      $password_confirm.removeClass('error');
+    } else {
+      $password.removeClass('good');
+      $password_confirm.removeClass('good');
+
+      $password.addClass('error');
+      $password_confirm.addClass('error');
+    }
+  } else {
+    $this.addClass('error');
+    $this.removeClass('good');
+  }
+
+  if (
+    $nickname.val().length > 3
+    && validateEmail($email.val())
+    && $password.val() == $password_confirm.val()
+  ) {
+    $submit_button.prop('disabled', false);
+  } else {
+    $submit_button.prop('disabled', true);
+  }
+});
+
+// On désactive le bouton de validation du formulaire au chargement de la page
+$(function() {
+  $('#submit').prop('disabled', true);
+});
